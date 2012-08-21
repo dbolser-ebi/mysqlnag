39dc88553f47f788828f0c871ad70bf240ed2754
Dan Bolser <dan.bolser@google.com>
2012-08-21 11:43:55 +0100

    updated

--- nagwrap.sh
+++ nagwrap.sh
@@ -6,53 +6,60 @@ DB2=$2
 DB1=${DB1:?plz pass a database to sync plz}
 DB2=${DB2:-${DB1}}
 
+echo P5LIB: $PERL5LIB
+
 echo "FROM DATABASE: '$DB1'"
 echo " TO  DATABASE: '$DB2'"
 
-## SOURCE database details (ensro, not ensrw)
+# ## SOURCE database details (ensro, not ensrw)
 
-#S_DB=( $( mysql-cluster-production-1  details ) )
-#S_DB=( $( mysql-staging-1             details ) )
- S_DB=( $( mysql-devel-2               details ) )
-
-
-## TARGET database details (ensrw, not admin)
-
-#T_DB=( $( mysql-devel-2-ensrw              details ) )
-#T_DB=( $( mysql-cluster-production-1-ensrw details ) )
-#T_DB=( $( mysql-staging-1-ensrw            details ) )
- T_DB=( $( mysql-staging-pre-ensrw          details ) )
+# #S_DB=( $( mysql-cluster-production-1  details ) )
+#  S_DB=( $( mysql-devel-2               details ) )
+# #S_DB=( $( mysql-staging-1             details ) )
 
 
-## Assign variables from the above 'details'
+# ## TARGET database details (ensrw, not admin)
 
-## These indexes differ from the below because 'ensro' has a different
-## number of arguments!
-S_HOST=${S_DB[4]}
-S_PORT=${S_DB[6]}
-S_USER=${S_DB[8]}
-S_PASS=${S_DB[10]}
-
-## These indexes differ from the above because 'ensrw' has a different
-## number of arguments.
-T_HOST=${T_DB[5]}
-T_PORT=${T_DB[7]}
-T_USER=${T_DB[9]}
-T_PASS=${T_DB[11]}
-
-#echo "FROM '$S_HOST' '$S_PORT' '$S_USER' '$S_PASS' OK"
-#echo "TO   '$T_HOST' '$T_PORT' '$T_USER' '$T_PASS' OK"
-#exit
+# #T_DB=( $( mysql-cluster-production-1-ensrw details ) )
+# #T_DB=( $( mysql-devel-2-ensrw              details ) )
+# #T_DB=( $( mysql-staging-1-ensrw            details ) )
+#  T_DB=( $( mysql-staging-pre-ensrw          details ) )
 
 
+# ## Assign variables from the above 'details'
 
-## RUN
+# ## These indexes differ from the below because 'ensro' has a different
+# ## number of arguments!
+# S_HOST=${S_DB[4]}
+# S_PORT=${S_DB[6]}
+# S_USER=${S_DB[8]}
+# S_PASS=${S_DB[10]}
+
+# ## These indexes differ from the above because 'ensrw' has a different
+# ## number of arguments.
+# T_HOST=${T_DB[5]}
+# T_PORT=${T_DB[7]}
+# T_USER=${T_DB[9]}
+# T_PASS=${T_DB[11]}
+
+# #echo "FROM '$S_HOST' '$S_PORT' '$S_USER' '$S_PASS' OK"
+# #echo "TO   '$T_HOST' '$T_PORT' '$T_USER' '$T_PASS' OK"
+# #exit
 
 
+
+
+#    --date \
+#    --checksum \
+#    --count \
+
+#    --no-dump \
+#    --table meta \
 
 time \
-  ./bin/mysqlnag --verbose --dir Logs \
-    --date \
+  ./bin/mysqlnag --dir Logs \
+    --count \
+    --verbose \
     ${FROM}     --database $DB1 \
     ${TO} --targetdatabase $DB2
 
